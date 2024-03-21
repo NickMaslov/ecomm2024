6c1fdadc8fbb5c96156d0b587ec169d587e2a48e
Nick Maslov <kategorychnost@gmail.com>
2024-03-21 14:48:42 -0400

    ssetting express server and connecting to frontend with axios

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,11 +1,22 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import { useParams, Link } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
 import Rating from '../components/Rating';
-import products from '../products';
 
 const ProductScreen = () => {
     const { id: productId } = useParams();
-    const product = products.find((p) => p._id === productId);
+    const [product, setProduct] = useState({});
+
+    useEffect(() => {
+        const fetchProduct = async () => {
+            const { data } = await axios.get(`/api/products/${productId}`);
+            console.log(data);
+            setProduct(data);
+        };
+
+        fetchProduct();
+    }, [productId]);
 
     return (
         <>
