2ac74127c43927fcaeb2049c92db97517c766076
Nick Maslov <kategorychnost@gmail.com>
2024-03-22 19:45:23 -0400

    setting up the MongoDB, seedin it, structuring and routing the backend

--- backend/server.js
+++ backend/server.js
@@ -2,23 +2,23 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import express from 'express';
-import products from './data/products.js';
-const port = process.env.PORT || 5000;
-
 const app = express();
+import connectDB from './config/db.js';
+connectDB();
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+
+import productRoutes from './routes/productRoutes.js';
+
+const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
     console.log('API is running...');
     res.send('API is running...');
 });
-app.get('/api/products', (req, res) => {
-    res.json(products);
-});
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((p) => p._id === req.params.id);
 
-    res.json(product);
-});
+app.use('/api/products', productRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () =>
     console.log(
