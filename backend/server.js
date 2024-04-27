ef9c9c27a13c3bd65252cab0589cd972d0c4f0bd
Nick Maslov <kategorychnost@gmail.com>
2024-04-27 03:11:43 -0400

    adding user routes, authentification, jwt on backend

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,33 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 dotenv.config();
 
-import express from "express";
+import express from 'express';
 const app = express();
-import connectDB from "./config/db.js";
-connectDB();
-import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-import productRoutes from "./routes/productRoutes.js";
+import connectDB from './config/db.js';
+connectDB();
+
+import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+
+import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 const port = process.env.PORT || 5001;
 
-app.get("/", (req, res) => {
-  console.log("API is running...");
-  res.send("API is running...");
+app.get('/', (req, res) => {
+    console.log('API is running...');
+    res.send('API is running...');
 });
 
-app.use("/api/products", productRoutes);
+app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
 app.listen(port, () =>
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
+    console.log(
+        `Server running in ${process.env.NODE_ENV} mode on port ${port}`
+    )
 );
