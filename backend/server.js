bf2c7ba4236c1a0495a4737140feb63b916a5e60
Nick Maslov <kategorychnost@gmail.com>
2024-05-08 23:58:37 -0400

    adding user routes, controllers, jwt, jwt-http-only

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,31 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
+import express from "express";
+import cookieParser from "cookie-parser";
+import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+const port = process.env.PORT || 5001;
+
 dotenv.config();
 
-import express from 'express';
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
-import connectDB from './config/db.js';
 connectDB();
 
-import { notFound, errorHandler } from './middleware/errorMiddleware.js';
-
-import productRoutes from './routes/productRoutes.js';
-import userRoutes from './routes/userRoutes.js';
-
-const port = process.env.PORT || 5001;
-
-app.get('/', (req, res) => {
-    console.log('API is running...');
-    res.send('API is running...');
+app.get("/", (req, res) => {
+  console.log("API is running...");
+  res.send("API is running...");
 });
 
-app.use('/api/products', productRoutes);
-app.use('/api/users', userRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
 app.listen(port, () =>
-    console.log(
-        `Server running in ${process.env.NODE_ENV} mode on port ${port}`
-    )
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`)
 );
